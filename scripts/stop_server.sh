87f1bc1aa3b3784c1675e8eccfed704dfc690236
Elena <pocpat@gmail.com>
2025-05-30 05:53:31 +0000

    Adding CodeDelpoy files

---
+++ scripts/stop_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+isExistApp="$(pgrep httpd)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop httpd.service
+fi
+isExistApp="$(pgrep tomcat)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop tomcat.service
+fi
+
+
